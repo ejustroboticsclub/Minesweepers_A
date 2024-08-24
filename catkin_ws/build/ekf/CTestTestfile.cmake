59416fea98051eef9d41b39f9659909ceb41c6a7
ejustroboticsclub <130171279+ejustroboticsclub@users.noreply.github.com>
2024-08-24 17:14:43 +0300

    Final updates

--- catkin_ws/build/ekf/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmed/catkin_ws/src/ekf
-# Build directory: /home/ahmed/catkin_ws/build/ekf
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- catkin_ws/build/rosserial/rosserial_msgs/catkin_generated/rosserial_msgs-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(rosserial_msgs_MESSAGE_FILES "/home/ahmed/catkin_ws/src/rosserial/rosserial_msgs/msg/Log.msg;/home/ahmed/catkin_ws/src/rosserial/rosserial_msgs/msg/TopicInfo.msg")
-set(rosserial_msgs_SERVICE_FILES "/home/ahmed/catkin_ws/src/rosserial/rosserial_msgs/srv/RequestParam.srv")
